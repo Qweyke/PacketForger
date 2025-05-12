ce36098f826f5b612b1185723b286f4ab56be7bd
Qweyke <kettenkrad1337@yandex.ru>
2025-05-12 21:19:19 +0500

    grok redone with gpt

--- stego_client.py
+++ stego_client.py
@@ -1,17 +1,16 @@
-import socket
+# NOTE: No socket.socket used! Full packet control with Scapy only
 from random import randrange
+from time import sleep
 
 from bitarray import bitarray
-from bitarray.util import int2ba
+from scapy.all import send, sniff
 from scapy.layers.inet import IP, TCP
-from scapy.sendrecv import send, sniff
 
 from custom_logger import dpi_logger
 from session_info import Port, TcpFlag, MAGIC_SEQ, CRC8_FUNC, BYTE_LEN_IN_BITS, CRC_LEN_BYTE, MSG_LEN_BYTE, \
     MAGIC_LEN_BYTE
 
 MAX_MSG_SIZE = (1 << 16) - 1
-LSB_MASK = int(~1)
 
 
 class StegoClient:
@@ -22,7 +21,6 @@ class StegoClient:
         self._srv = None
         self._seq = None
         self._ack = None
-        self._client_socket = None
 
     def _build_init_seq(self, msg: str) -> int | None:
         msg_len_in_bits = len(msg.encode("utf-8")) * BYTE_LEN_IN_BITS
@@ -33,112 +31,80 @@ class StegoClient:
             return None
 
         magic_masked = MAGIC_SEQ << (MSG_LEN_BYTE * BYTE_LEN_IN_BITS)
-        dpi_logger.debug(
-            f"Magic converted: {int2ba(magic_masked)}, len {len(int2ba(magic_masked))}. Magic initial: {int2ba(MAGIC_SEQ)}, len {len(int2ba(MAGIC_SEQ))}")
-
         base_seq = magic_masked | msg_len_in_bits
-        crc_int = CRC8_FUNC(base_seq.to_bytes(MAGIC_LEN_BYTE + MSG_LEN_BYTE, "big"))
-
-        dpi_logger.debug(f"CRC: {crc_int}, bits {int2ba(crc_int)}, len {len(int2ba(crc_int))}")
-
+        crc_int = CRC8_FUNC(base_seq.to_bytes(MSG_LEN_BYTE + MAGIC_LEN_BYTE, "big"))
         full_seq = (base_seq << CRC_LEN_BYTE * BYTE_LEN_IN_BITS) | crc_int
-        dpi_logger.debug(f"Full seq: {full_seq}, bits: {int2ba(full_seq)}, len {len(int2ba(full_seq))}")
-
         return full_seq
 
     def _receive_init_syn_ack(self):
         timeout = 3
 
-        def is_synack_reply(packet):
-            if (
-                    packet.haslayer(TCP)
-                    and packet.haslayer(IP)
-                    and packet[IP].src == self._srv
-                    and packet[IP].dst == self._clt
-                    and packet[TCP].sport == Port.HTTP.value
-                    and packet[TCP].dport == self._curr_port
-                    and packet[TCP].flags == (TcpFlag.SYN.value | TcpFlag.ACK.value)
-                    and packet[TCP].ack == self._seq + 1
-            ):
-                return True
-            return False
+        def is_synack_reply(pkt):
+            return (
+                    pkt.haslayer(TCP)
+                    and pkt.haslayer(IP)
+                    and pkt[IP].src == self._srv
+                    and pkt[IP].dst == self._clt
+                    and pkt[TCP].sport == Port.HTTP.value
+                    and pkt[TCP].dport == self._curr_port
+                    and pkt[TCP].flags == (TcpFlag.SYN.value | TcpFlag.ACK.value)
+                    and pkt[TCP].ack == self._seq + 1
+            )
 
         response = sniff(lfilter=is_synack_reply, count=1, timeout=timeout)
-
         if response:
             pkt = response[0]
-            dpi_logger.debug(f"Got SYN-ACK. SEQ = {pkt[TCP].seq}, ACK = {pkt[TCP].ack}")
-            self._ack = pkt[TCP].seq + 1
             self._seq += 1
-            tcp_l = TCP(sport=self._curr_port, dport=Port.HTTP.value, seq=self._seq, ack=self._ack,
-                        flags=TcpFlag.ACK.value)
-            ack_pkt = IP(src=self._clt, dst=self._srv) / tcp_l
-            send(ack_pkt)
-            dpi_logger.debug(f"Sent ACK: seq={self._seq}, ack={self._ack}")
+            self._ack = pkt[TCP].seq + 1
+
+            ack_pkt = IP(src=self._clt, dst=self._srv) / TCP(
+                sport=self._curr_port, dport=Port.HTTP.value,
+                seq=self._seq, ack=self._ack, flags=TcpFlag.ACK.value
+            )
+            send(ack_pkt, verbose=0)
             return True
         else:
-            dpi_logger.error(f"ACK from server wasn't received after timeout '{timeout}' secs")
+            dpi_logger.error("ACK from server wasn't received")
             return False
-
-    def _send_fin(self):
-        tcp_l = TCP(sport=self._curr_port, dport=Port.HTTP.value, seq=self._seq, ack=self._ack,
-                    flags=TcpFlag.FIN.value | TcpFlag.ACK.value)
-        fin_pkt = IP(src=self._clt, dst=self._srv) / tcp_l
-        send(fin_pkt)
-        dpi_logger.info("Sent FIN packet to close connection")
 
     def send_stego_msg(self, msg: str, clt_ip: str, srv_ip: str):
         self._clt = clt_ip
         self._srv = srv_ip
-        self._curr_port = None
-        self._client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-        # Цикл для поиска свободного порта
-        while True:
-            self._curr_port = randrange(49152, 65535)
-            try:
-                self._client_socket.bind((self._clt, self._curr_port))
-                dpi_logger.info(f"Client bound to {self._clt}:{self._curr_port}")
-                break
-            except Exception as e:
-                dpi_logger.warning(f"Port {self._curr_port} is in use, trying another: {e}")
+        self._curr_port = randrange(49152, 65535)
 
         init_seq = self._build_init_seq(msg)
         if init_seq is None:
-            self._client_socket.close()
             return
 
         self._seq = init_seq
-        tcp_l = TCP(sport=self._curr_port, dport=Port.HTTP.value, seq=self._seq, flags=TcpFlag.SYN.value)
-        init_pkt = IP(src=self._clt, dst=self._srv) / tcp_l
-        send(init_pkt)
-        dpi_logger.info("Sent SYN packet with init sequence")
+        syn_pkt = IP(src=self._clt, dst=self._srv) / TCP(
+            sport=self._curr_port, dport=Port.HTTP.value,
+            seq=self._seq, flags=TcpFlag.SYN.value
+        )
+        send(syn_pkt, verbose=0)
+        dpi_logger.info("Sent SYN with embedded data")
 
-        if self._receive_init_syn_ack():
-            dpi_logger.info("Start transmission!")
-            bits_seq = bitarray()
-            bits_seq.frombytes(msg.encode("utf-8"))
+        if not self._receive_init_syn_ack():
+            return
 
-            for i, bit in enumerate(bits_seq):
-                self._seq += 1  # Увеличиваем seq для каждого пакета
-                if bit == 1:
-                    self._seq |= bit
-                else:
-                    self._seq &= ~1
+        # Begin covert transmission of message bits in SEQ
+        bits = bitarray()
+        bits.frombytes(msg.encode("utf-8"))
 
-                tcp_l = TCP(sport=self._curr_port, dport=Port.HTTP.value, seq=self._seq,
-                            flags=TcpFlag.PSH.value | TcpFlag.ACK.value)
-                # Добавляем фиктивную нагрузку
-                init_pkt = IP(src=self._clt, dst=self._srv) / tcp_l / "ping"
-                send(init_pkt)
-                dpi_logger.debug(f"Sent bit {bit} with seq {self._seq}")
+        for i, bit in enumerate(bits):
+            self._seq += 2
+            self._seq = (self._seq & ~1) | bit
 
-            # Завершаем соединение
-            self._send_fin()
+            pkt = IP(src=self._clt, dst=self._srv) / TCP(
+                sport=self._curr_port, dport=Port.HTTP.value,
+                seq=self._seq, ack=self._ack,
+                flags=TcpFlag.PSH.value | TcpFlag.ACK.value
+            )
+            send(pkt, verbose=0)
+            dpi_logger.debug(f"Sent bit {bit} with seq {self._seq}")
+            sleep(0.05)
 
-        self._client_socket.close()
-        dpi_logger.info("Client socket closed")
+        dpi_logger.info("Transmission complete")
 
 
 if __name__ == "__main__":
--- stego_server.py
+++ stego_server.py
@@ -1,15 +1,10 @@
-import socket
-import threading
 from random import randint
-from typing import Any
 
 from bitarray import bitarray
-from scapy.all import sniff, IP, TCP, Ether
+from scapy.all import sniff, Raw, sendp
 from scapy.config import conf
-from scapy.layers.inet import TCP, IP
+from scapy.layers.inet import IP, TCP
 from scapy.layers.l2 import Ether
-from scapy.packet import Packet
-from scapy.sendrecv import sendp
 
 from custom_logger import dpi_logger
 from session_info import Port, TcpFlag, MAGIC_SEQ, \
@@ -17,9 +12,7 @@ from session_info import Port, TcpFlag, MAGIC_SEQ, \
     MSG_LEN_BYTE, get_target_mac
 
 conf.debug_dissector = 2
-
 MAX_TCP_SEQ_NUM = (1 << 32) - 1
-DATA_SIZE_IN_BYTES = 2048
 
 
 class StegoServer:
@@ -29,184 +22,114 @@ class StegoServer:
         self._msg_len = 0
         self._captured_bits = bitarray()
         self._packet_cnt = 0
-        self._used_seqs = []
+        self._used_seqs = set()
         self._srv_ip = None
         self._clt_ip = None
         self._clt_port = None
-        self._server_socket = None
         self._last_ack = 0
 
-    def _handle_client_conn(self, conn: socket.socket, addr: Any):
-        dpi_logger.info(f"Connection received from {addr}")
-        try:
-            data = conn.recv(DATA_SIZE_IN_BYTES)
-            if not data:
-                dpi_logger.info(f"Client {addr} disconnected")
-                return
-            if b"GET" in data:
-                dpi_logger.info(f"[>] Normal HTTP request from {addr}")
-                conn.sendall(b"HTTP/1.1 200 OK\r\nContent-Length: 3\r\n\r\n\nOK\n")
-            else:
-                dpi_logger.info(f"[?] Unknown request from {addr}")
-                conn.sendall(b"HTTP/1.1 400 Bad Request\r\n\r\n")
-        finally:
-            conn.close()
+    def _send_tcp_pkt(self, flags, seq, ack, payload=b""):
+        ip_l = IP(src=self._srv_ip, dst=self._clt_ip)
+        tcp_l = TCP(
+            sport=Port.HTTP.value,
+            dport=self._clt_port,
+            flags=flags,
+            seq=seq,
+            ack=ack
+        )
+        ether = Ether(dst=get_target_mac(self._clt_ip))
+        pkt = ether / ip_l / tcp_l / Raw(load=payload) if payload else ether / ip_l / tcp_l
+        sendp(pkt, verbose=False)
 
     def _handle_transmission_init(self, seq_num, sport, src_ip, dst_ip):
-        dpi_logger.debug(f"Sequence value: {seq_num}")
         magic_trimmed = seq_num >> ((MAGIC_LEN_BYTE + MSG_LEN_BYTE) * BYTE_LEN_IN_BITS) & 0xFF
         msg_len_trimmed = (seq_num >> (CRC_LEN_BYTE * BYTE_LEN_IN_BITS)) & 0xFFFF
         seq_base = (seq_num >> CRC_LEN_BYTE * BYTE_LEN_IN_BITS) & 0xFFFFFF
         crc_received = seq_num & 0xFF
 
-        dpi_logger.debug(f"Magic: 0x{magic_trimmed:02x}, Msg_len: {msg_len_trimmed}, CRC: 0x{crc_received:02x}")
-
         if magic_trimmed == MAGIC_SEQ:
-            dpi_logger.warning("* * * Stego channel request detected * * *")
             crc_calculated = CRC8_FUNC(seq_base.to_bytes(MAGIC_LEN_BYTE + MSG_LEN_BYTE, "big"))
             if crc_calculated == crc_received:
-                dpi_logger.warning(f"CRC correct, accepting msg_len={msg_len_trimmed}")
-                self._used_seqs.append(seq_num)
+                self._used_seqs.add(seq_num)
                 self._stego_active = True
                 self._msg_len = msg_len_trimmed
                 self._clt_port = sport
                 self._clt_ip = src_ip
                 self._srv_ip = dst_ip
 
-                # Отправляем SYN-ACK
-                ip_l = IP(src=self._srv_ip, dst=self._clt_ip)
-                tcp_l = TCP(
-                    sport=Port.HTTP.value,
-                    dport=self._clt_port,
-                    flags="SA",
-                    seq=self._server_seq,
-                    ack=seq_num + 1
-                )
-                mac = get_target_mac(self._clt_ip)
-                sa_pkt = Ether(dst=mac) / ip_l / tcp_l
-                sendp(sa_pkt)
-                dpi_logger.info(f"Sent SYN-ACK: seq={self._server_seq}, ack={seq_num + 1}")
+                self._send_tcp_pkt(flags="SA", seq=self._server_seq, ack=seq_num + 1)
+                dpi_logger.info(f"[*] Stego SYN-ACK sent: seq={self._server_seq}, ack={seq_num + 1}")
                 self._last_ack = seq_num + 1
             else:
-                dpi_logger.error("CRC incorrect, rejecting")
-        else:
-            dpi_logger.debug("No magic sequence")
+                dpi_logger.warning("[-] CRC mismatch, stego rejected")
 
-    def _handle_stego_packet(self, packet: Packet):
-        if packet.haslayer(TCP):
-            dpi_logger.debug("Received a packet")
-            tcp_layer = packet.getlayer(TCP)
-            seq_num = tcp_layer.seq
-            src_ip = packet.getlayer(IP).src
-            dst_ip = packet.getlayer(IP).dst
+    def _handle_stego_packet(self, packet):
+        if not packet.haslayer(TCP):
+            return
 
-            if seq_num in self._used_seqs:
-                dpi_logger.debug("Packet's TCP sequence already used, skipping...")
+        tcp = packet[TCP]
+        ip = packet[IP]
+        src_ip = ip.src
+        dst_ip = ip.dst
+        seq_num = tcp.seq
+        sport = tcp.sport
+
+        if seq_num in self._used_seqs:
+            return
+
+        if tcp.flags == TcpFlag.SYN.value:
+            self._handle_transmission_init(seq_num, sport, src_ip, dst_ip)
+            return
+
+        # Маскируемся под HTTP, если получен обычный GET
+        if tcp.flags & TcpFlag.PSH.value and packet.haslayer(Raw):
+            data = packet[Raw].load
+            if data.startswith(b"GET"):
+                dpi_logger.info("[>] Received normal GET, responding with fake HTTP")
+                self._clt_ip = src_ip
+                self._clt_port = sport
+                self._srv_ip = dst_ip
+                fake_http = b"HTTP/1.1 200 OK\r\nContent-Length: 3\r\n\r\nOK\n"
+                self._send_tcp_pkt(flags="PA", seq=self._server_seq, ack=seq_num + len(data), payload=fake_http)
+                self._server_seq += len(fake_http)
                 return
 
-            if tcp_layer.flags == TcpFlag.SYN.value:
-                self._handle_transmission_init(seq_num=seq_num, sport=tcp_layer.sport, src_ip=src_ip, dst_ip=dst_ip)
+        if self._stego_active and (tcp.flags & TcpFlag.ACK.value or tcp.flags & TcpFlag.PSH.value):
+            self._used_seqs.add(seq_num)
+            bit = seq_num & 1
+            self._captured_bits.append(bit)
+            self._packet_cnt += 1
+            dpi_logger.debug(f"[<] Received bit: {bit} ({self._packet_cnt}/{self._msg_len})")
 
-            elif self._stego_active and (tcp_layer.flags == TcpFlag.ACK.value or tcp_layer.flags & TcpFlag.PSH.value):
-                self._used_seqs.append(seq_num)
-                extracted_bit = seq_num & 1
-                self._captured_bits.append(extracted_bit)
-                self._packet_cnt += 1
-                dpi_logger.debug(f"Extracted bit: {extracted_bit}, packet count: {self._packet_cnt}")
+            self._send_tcp_pkt(flags="A", seq=self._server_seq, ack=seq_num + 1)
+            self._server_seq += 1
+            self._last_ack = seq_num + 1
 
-                if tcp_layer.flags & TcpFlag.PSH.value:
-                    ip_l = IP(src=self._srv_ip, dst=self._clt_ip)
-                    tcp_l = TCP(
-                        sport=Port.HTTP.value,
-                        dport=self._clt_port,
-                        flags="A",
-                        seq=self._server_seq,
-                        ack=seq_num + 1  # Увеличиваем ack для подтверждения PSH
-                    )
-                    mac = get_target_mac(self._clt_ip)
-                    ack_pkt = Ether(dst=mac) / ip_l / tcp_l
-                    sendp(ack_pkt)
-                    dpi_logger.debug(f"Sent ACK for PSH: seq={self._server_seq}, ack={seq_num + 1}")
-                    self._server_seq += 1
-                    self._last_ack = seq_num + 1
+            if self._packet_cnt >= self._msg_len:
+                msg = self._captured_bits.tobytes().decode("utf-8", errors="ignore")
+                dpi_logger.warning(f"[!] Message received: '{msg}'")
 
-                if self._packet_cnt == self._msg_len:
-                    msg_in_bytes = self._captured_bits.tobytes()
-                    message = msg_in_bytes.decode('utf-8')
-                    dpi_logger.info(f"Message received: {message}")
-                    self._stego_active = False
-
-                    # Отправляем FIN для завершения соединения
-                    ip_l = IP(src=self._srv_ip, dst=self._clt_ip)
-                    tcp_l = TCP(
-                        sport=Port.HTTP.value,
-                        dport=self._clt_port,
-                        flags="FA",
-                        seq=self._server_seq,
-                        ack=self._last_ack
-                    )
-                    mac = get_target_mac(self._clt_ip)
-                    fin_pkt = Ether(dst=mac) / ip_l / tcp_l
-                    sendp(fin_pkt)
-                    dpi_logger.info("Sent FIN packet to close connection")
-
-                    self._captured_bits = bitarray()
-                    self._used_seqs.clear()
-                    self._packet_cnt = 0
-                    self._clt_port = None
-
-            elif tcp_layer.flags & TcpFlag.FIN.value:
-                # Подтверждаем FIN клиента
-                ip_l = IP(src=self._srv_ip, dst=self._clt_ip)
-                tcp_l = TCP(
-                    sport=Port.HTTP.value,
-                    dport=self._clt_port,
-                    flags="FA",
-                    seq=self._server_seq,
-                    ack=seq_num + 1
-                )
-                mac = get_target_mac(self._clt_ip)
-                fin_ack_pkt = Ether(dst=mac) / ip_l / tcp_l
-                sendp(fin_ack_pkt)
-                dpi_logger.info("Received FIN, sent FIN-ACK")
+                self._send_tcp_pkt(flags="FA", seq=self._server_seq, ack=self._last_ack)
                 self._server_seq += 1
 
-        else:
-            dpi_logger.debug("Corrupted packet")
+                # reset state
+                self._stego_active = False
+                self._captured_bits.clear()
+                self._used_seqs.clear()
+                self._packet_cnt = 0
 
-    def start_server(self, host="192.168.12.4", port=Port.HTTP.value):
-        self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self._server_socket.bind((host, port))
-        self._server_socket.listen(5)
-        dpi_logger.info(f"[+] Listening on {host}:{port}")
+        elif tcp.flags & TcpFlag.FIN.value:
+            dpi_logger.info("[*] FIN received from client, sending FIN-ACK")
+            self._send_tcp_pkt(flags="FA", seq=self._server_seq, ack=seq_num + 1)
+            self._server_seq += 1
 
-        try:
-            while True:
-                conn, addr = self._server_socket.accept()
-                threading.Thread(target=self._handle_client_conn, args=(conn, addr), daemon=True).start()
-        except KeyboardInterrupt:
-            dpi_logger.warning("\n[!] Initiating shutdown...")
-        finally:
-            if self._server_socket:
-                self._server_socket.close()
-                dpi_logger.warning("\n[!] Server is down.")
-
-    def start_sniffing(self, clt_ip: str = "0.0.0.0", srv_ip: str = "0.0.0.0"):
+    def start_sniffing(self, clt_ip="0.0.0.0", srv_ip="0.0.0.0"):
         self._srv_ip = srv_ip
         self._clt_ip = clt_ip
         iface = search_for_ifaces()
-        filter_berk = f"port {Port.HTTP.value} and src host {self._clt_ip} and dst host {self._srv_ip}"
-        dpi_logger.info(
-            f"* * * Server is listening for hidden transmission on '{iface}' with filter '{filter_berk}' * * *")
-
-        server_thread = threading.Thread(target=self.start_server, args=("192.168.12.4", Port.HTTP.value), daemon=True)
-        server_thread.start()
-
-        sniff(iface=iface,
-              filter=filter_berk,
-              prn=self._handle_stego_packet,
-              store=False)
+        filter_expr = f"tcp port {Port.HTTP.value} and src host {self._clt_ip} and dst host {self._srv_ip}"
+        dpi_logger.info(f"[~] Sniffing on {iface} with filter: {filter_expr}")
+        sniff(iface=iface, filter=filter_expr, prn=self._handle_stego_packet, store=False)
 
 
 if __name__ == "__main__":
